068dcbad6f5eec3ce84f79bd83d616493952a104
RegiusPythonidae <kvara9990@yahoo.com>
2021-02-23 18:55:27 +0400

    Still fixing stuff for Heroku

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, redirect
+from flask_restful import Api
+from security import *
+from flask_jwt import JWT
+from resources.item import *
+from resources.user import *
+from db import db
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '91939'
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+api = Api(app)
+jwt = JWT(app, authenticate, identity)
+
+api.add_resource(ItemList, "/items")
+api.add_resource(Item, "/items/<int:product_id>")
+api.add_resource(RegisterUser, "/registration")
+
+@app.route("/")
+def home():
+    return redirect("https://github.com/RegiusPythonidae/unilab_internship_project/tree/main"), 302
+
+if __name__ == '__main__':
+
+    app.run()
--- run.py
+++ run.py
@@ -1,35 +1,8 @@
-from flask import Flask, redirect
-from flask_restful import Api
-from security import *
-from flask_jwt import JWT
-from resources.item import *
-from resources.user import *
+from app import app
 from db import db
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '91939'
-app.config['PROPAGATE_EXCEPTIONS'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-
-
-api = Api(app)
-jwt = JWT(app, authenticate, identity)
 db.init_app(app)
 
 @app.before_first_request
 def initialize_tables():
     db.create_all()
-
-
-api.add_resource(ItemList, "/items")
-api.add_resource(Item, "/items/<int:product_id>")
-api.add_resource(RegisterUser, "/registration")
-
-@app.route("/")
-def home():
-    return redirect("https://github.com/RegiusPythonidae/unilab_internship_project/tree/main"), 302
-
-if __name__ == '__main__':
-
-    app.run()
